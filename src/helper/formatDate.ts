c8b178007a795ea6a59166c3467182d2c1bdeb8d
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-03-18 16:30:55 +0700

    Change function formatedate

--- src/helper/formatDate.ts
+++ src/helper/formatDate.ts
@@ -1,5 +1,4 @@
 export function formatDate(dateString: string) {
     const date = new Date(dateString);
-    const options = { day: '2-digit', month: 'long', year: 'numeric' };
-    return date.toLocaleDateString('en-GB', options);
-  }
+    return date.toLocaleDateString('en-GB', { day: '2-digit', month: 'long', year: 'numeric' });
+}
